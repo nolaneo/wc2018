221f0ab545e5a83ec3d4c9fc8fb53eb59253f0fa
Eoin Nolan <nolan.eoin@gmail.com>
2018-06-12 21:14:00 +0100

    Fleshing it out

---
+++ app/data/teams.js
@@ -0,0 +1,162 @@
+export default [
+  {
+    "team": "Brazil",
+    "pool": "E",
+    "odds": "5.5"
+  },
+  {
+    "team": "Germany",
+    "pool": "F",
+    "odds": "5.5"
+  },
+  {
+    "team": "Spain",
+    "pool": "B",
+    "odds": "6.5"
+  },
+  {
+    "team": "France",
+    "pool": "C",
+    "odds": "7.0"
+  },
+  {
+    "team": "Belgium",
+    "pool": "G",
+    "odds": "11.0"
+  },
+  {
+    "team": "Argentina",
+    "pool": "D",
+    "odds": "11.0"
+  },
+  {
+    "team": "England",
+    "pool": "G",
+    "odds": "17.0"
+  },
+  {
+    "team": "Portugal",
+    "pool": "B",
+    "odds": "23.0"
+  },
+  {
+    "team": "Uruguay",
+    "pool": "A",
+    "odds": "26.0"
+  },
+  {
+    "team": "Croatia",
+    "pool": "D",
+    "odds": "34.0"
+  },
+  {
+    "team": "Colombia",
+    "pool": "H",
+    "odds": "34.0"
+  },
+  {
+    "team": "Russia",
+    "pool": "A",
+    "odds": "41.0"
+  },
+  {
+    "team": "Poland",
+    "pool": "H",
+    "odds": "51.0"
+  },
+  {
+    "team": "Denmark",
+    "pool": "C",
+    "odds": "81.0"
+  },
+  {
+    "team": "Mexico",
+    "pool": "F",
+    "odds": "101.0"
+  },
+  {
+    "team": "Switzerland",
+    "pool": "E",
+    "odds": "101.0"
+  },
+  {
+    "team": "Serbia",
+    "pool": "E",
+    "odds": "126.0"
+  },
+  {
+    "team": "Sweden",
+    "pool": "F",
+    "odds": "151.0"
+  },
+  {
+    "team": "Egypt",
+    "pool": "A",
+    "odds": "151.0"
+  },
+  {
+    "team": "Peru",
+    "pool": "C",
+    "odds": "201.0"
+  },
+  {
+    "team": "Nigeria",
+    "pool": "D",
+    "odds": "201.0"
+  },
+  {
+    "team": "Iceland",
+    "pool": "D",
+    "odds": "201.0"
+  },
+  {
+    "team": "Senegal",
+    "pool": "H",
+    "odds": "201.0"
+  },
+  {
+    "team": "Japan",
+    "pool": "H",
+    "odds": "251.0"
+  },
+  {
+    "team": "Australia",
+    "pool": "C",
+    "odds": "276.0"
+  },
+  {
+    "team": "Morocco",
+    "pool": "B",
+    "odds": "326.0"
+  },
+  {
+    "team": "Iran",
+    "pool": "B",
+    "odds": "501.0"
+  },
+  {
+    "team": "Costa Rica",
+    "pool": "E",
+    "odds": "501.0"
+  },
+  {
+    "team": "South Korea",
+    "pool": "F",
+    "odds": "501.0"
+  },
+  {
+    "team": "Tunisia",
+    "pool": "G",
+    "odds": "501.0"
+  },
+  {
+    "team": "Panama",
+    "pool": "G",
+    "odds": "501.0"
+  },
+  {
+    "team": "Saudi Arabia",
+    "pool": "A",
+    "odds": "501.0"
+  }
+];
