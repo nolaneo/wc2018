ce9ef85336becf9611f8b301089adc3f6e5febbd
Eoin Nolan <nolan.eoin@gmail.com>
2018-06-15 20:38:25 +0100

    css spinner

--- app/components/match-list.js
+++ app/components/match-list.js
@@ -6,7 +6,6 @@ export default Component.extend({
   ajax: inject(),
 
   apiURL: 'https://world-cup-json.herokuapp.com/matches',
-  spinner: 'wc2018/master/public/assets/images/loading-spinner',
 
   didInsertElement() {
     this._super(...arguments);
