19c50476cff0e88bfc3d4b05895a2b11560ed2e6
Eoin Nolan <nolan.eoin@gmail.com>
2018-06-15 18:38:00 +0100

    Use api

--- app/components/match-list.js
+++ app/components/match-list.js
@@ -1,20 +1,36 @@
 import Component from '@ember/component';
-import matches from 'wc2018/data/matches';
+import { task } from 'ember-concurrency';
+import { inject } from '@ember/service';
 
 export default Component.extend({
+  ajax: inject(),
+
+  apiURL: 'http://worldcup.sfg.io/matches',
+
   didInsertElement() {
     this._super(...arguments);
+    this.get('fetchMatches').perform();
     let teamToPlayerSet = {};
     this.get('players').map(player => {
       player.teams.map(team => teamToPlayerSet[team] = player.name);
     });
     this.set('teamToPlayerSet', teamToPlayerSet);
-    let fixtures = matches.map(match => {
-      let fixture = Object.assign({}, match);
-      fixture.homePlayer = teamToPlayerSet[match.homeTeam];
-      fixture.awayPlayer = teamToPlayerSet[match.awayTeam];
-      return fixture;
+  },
+
+  fetchMatches: task(function * () {
+    let data = yield this.get('ajax').request(this.get('apiURL'));
+    let matches = data
+      .filter(match => match.status === "future")
+      .filter(match => match.home_team.code !== "TBD" || match.away_team.code !== "TBD");
+    this.set('matches', this.injectPlayerName(matches));
+  }),
+
+  injectPlayerName(matchArray) {
+    return matchArray.map(match => {
+      match.home_team.player_name = this.get('teamToPlayerSet')[match.home_team.country];
+      match.away_team.player_name = this.get('teamToPlayerSet')[match.away_team.country];
+      return match;
     })
-    this.set('fixtures', fixtures);
-  }
+  },
+
 });
