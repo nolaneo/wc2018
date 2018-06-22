1e45e627907a606ab731b6085b3899fb8f0ef9d8
Eoin Nolan <nolan.eoin@gmail.com>
2018-06-22 22:18:50 +0100

    add stats

--- app/components/match-stats.js
+++ app/components/match-stats.js
@@ -2,7 +2,9 @@ import Component from '@ember/component';
 import { computed } from '@ember/object';
 
 export default Component.extend({
-  classNames: ['layout__box', 'o__flexes-to-1', 'o__has-rows', 'u__pad__20'],
+  classNames: ['layout__box', 'o__flexes-to-1', 'o__has-rows', 'u__pad__50', 'stat-table'],
+  classNameBindings: ['hasNoEvents:u__hidden'],
+  hasNoEvents: computed.empty('events'),
   events: computed('match.{home_team_events,away_team_events}', function() {
     this.get('match.home_team_events').setEach('side', 'home');
     this.get('match.away_team_events').setEach('side', 'away');
