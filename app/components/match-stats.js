647e809389b8f44dd77b5ed0863912edb589be7e
Eoin Nolan <nolan.eoin@gmail.com>
2018-06-22 21:12:17 +0100

    Add stats

---
+++ app/components/match-stats.js
@@ -0,0 +1,12 @@
+import Component from '@ember/component';
+import { computed } from '@ember/object';
+
+export default Component.extend({
+  classNames: ['layout__box', 'o__flexes-to-1', 'o__has-rows', 'u__pad__20'],
+  events: computed('match.{home_team_events,away_team_events}', function() {
+    this.get('match.home_team_events').setEach('side', 'home');
+    this.get('match.away_team_events').setEach('side', 'away');
+    let events = this.get('match.home_team_events').concat(this.get('match.away_team_events'));
+    return events.sortBy('time');
+  }),
+});
