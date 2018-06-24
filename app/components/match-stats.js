4c9516b78152d8721187d17f67eb05913bf7bab6
Eoin Nolan <nolan.eoin@gmail.com>
2018-06-24 12:55:41 +0100

    Fit stat duplication

--- app/components/match-stats.js
+++ app/components/match-stats.js
@@ -9,6 +9,28 @@ export default Component.extend({
     this.get('match.home_team_events').setEach('side', 'home');
     this.get('match.away_team_events').setEach('side', 'away');
     let events = this.get('match.home_team_events').concat(this.get('match.away_team_events'));
-    return events.sortBy('time');
+    events = this.dedupEvents(events);
+    return events.sortBy('integerTime');
   }),
+
+  dedupEvents(events) {
+    events = events.map(event => {
+      let time = event.time.replace(`'`, '').split('+');
+      let integerTime = Number.parseInt(time[0]) + (Number.parseInt(time[1]) || 0);
+      event.integerTime = integerTime;
+      return event;
+    })
+    events = events.reduce((accumulator, event) => {
+      let lastEvent = (accumulator.get('lastObject') || {});
+      if (lastEvent.player == event.player && lastEvent.type_of_event == event.type_of_event && (Math.abs(lastEvent.integerTime - event.integerTime) <= 1)) {
+        console.log(`Duplicate Event:`);
+        console.log(`${lastEvent.player} ${lastEvent.type_of_event} ${lastEvent.time}`);
+        console.log(`${event.player} ${event.type_of_event} ${event.time}`);
+      } else {
+        accumulator.pushObject(event);
+      }
+      return accumulator;
+    }, []);
+    return events;
+  }
 });
